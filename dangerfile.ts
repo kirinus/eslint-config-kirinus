eea9ef1a7b15db7e09b4b5853a97c033c7617497
Dario Blanco <dblancoit@gmail.com>
2021-06-23 20:39:13 +0200

    chore: add linters

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,4 +1,5 @@
-import { message, danger } from 'danger';
+import kirinus from 'danger-plugin-kirinus';
 
-const modifiedMD = danger.git.modified_files.join('- ');
-message('Changed Files in this PR: \n - ' + modifiedMD);
+(async function dangerReport() {
+  await kirinus();
+})();
